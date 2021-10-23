02ceb314d95f567330371172ddd64c2e21f882f6
lohba <lukeohba@gmail.com>
2021-10-23 07:16:49 -0400

    edits

---
+++ src/error.rs
@@ -0,0 +1,16 @@
+use thiserror::Error;
+use solana_program::program_error::ProgramError;
+//use crate::{instruction::EscrowInstruction, error::EscrowError};
+
+#[derive(Error, Debug, Copy, Clone)]
+pub enum EscrowError {
+    /// Invalid instruction
+    #[error("Invalid Instruction")]
+    InvalidInstruction,
+}
+
+impl From<EscrowError> for ProgramError {
+    fn from(e: EscrowError) -> Self {
+        ProgramError::Custom(e as u32)
+    }
+}
